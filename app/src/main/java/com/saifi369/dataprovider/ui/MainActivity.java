f81b0f563915026a0892ce5588eacaa576230a68
M Saif Ullah <chsaif369@gmail.com>
2019-06-29 01:08:20 +0500

    Contains Demo Project with ListView and Images for showing list of cities with their image in a ListView.

--- app/src/main/java/com/saifi369/dataprovider/ui/MainActivity.java
+++ app/src/main/java/com/saifi369/dataprovider/ui/MainActivity.java
@@ -1,100 +1,48 @@
 package com.saifi369.dataprovider.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.saifi369.dataprovider.R;
-import com.saifi369.dataprovider.adapters.MyDataAdapter;
-import com.saifi369.dataprovider.database.CityDataSource;
+import com.saifi369.dataprovider.adapters.MyListAdapter;
 import com.saifi369.dataprovider.model.CityDataItem;
+import com.saifi369.dataprovider.sample.SampleDataProvider;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private List<CityDataItem> mDataList;
-    private RecyclerView mRecyclerView;
-    private CityDataSource mDataSource;
+    private ListView mListView;
 
-    MyDataAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mDataList= SampleDataProvider.cityDataItemList;
-        mRecyclerView=findViewById(R.id.list_city);
+        mListView=findViewById(R.id.list_city);
+        mDataList= SampleDataProvider.cityDataItemList;
 
-        mDataSource=new CityDataSource(this);
-        mDataSource.open();
-        mDataSource.seedDatabase();
+        MyListAdapter adapter=new MyListAdapter(this,mDataList);
 
-        LinearLayoutManager manager=new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(manager);
-
-        getDatabaseList(null);
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.app_bar_menu,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()){
-            case R.id.show_all:
-            {
-                getDatabaseList(null);
-                break;
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String cityId=mDataList.get(position).getCityId();
+                Toast.makeText(MainActivity.this, cityId, Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this,DetailsActivity.class);
+                intent.putExtra(MyListAdapter.CITY_KEY,cityId);
+                startActivity(intent);
             }
-            case R.id.show_balochistan:
-            {
-                getDatabaseList("Balochistan");
-                break;
-            }
-            case R.id.show_kpk:
-            {
-                getDatabaseList("KPK");
-                break;
-            }
-            case R.id.show_punjab:
-            {
-                getDatabaseList("Punjab");
-                break;
-            }
-            case R.id.show_sindh:
-            {
-                getDatabaseList("Sindh");
-                break;
-            }
-        }
-        return super.onOptionsItemSelected(item);
+        });
+
+        mListView.setAdapter(adapter);
+
     }
 
-
-    private void getDatabaseList(String prov) {
-        mDataList=mDataSource.getAllItems(prov);
-        adapter=new MyDataAdapter(mDataList,this);
-        mRecyclerView.setAdapter(adapter);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mDataSource.open();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mDataSource.close();
-    }
 }
